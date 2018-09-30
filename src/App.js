95a233fdeb24d7671864de64c2c1d5f62867bd9a
Matthieu <ranuccimatthieu@gmail.com>
2018-09-30 13:36:35 +0200

    ajout Home History

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
+import Home from './Home';
+import History from './History';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <NavLink exact to="/"> Accueil </NavLink>
+                    <NavLink to="/notre-histoire"> Histoire </NavLink>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/notre-histoire" component={History} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
